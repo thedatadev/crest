b3b33d923f41c5e302eecbdea8db58674c775e01
thedatadev <thedatadev@gmail.com>
2019-04-14 18:11:12 +1000

    duplicate a repo in go

--- go_imp/README.md
+++ go_imp/README.md
@@ -2,10 +2,10 @@
 ### An exercise in writing command line utilities in Go
 
 TODO:
-- Write a program to walk the source repo and print out file/dir names
-- Modify the function to duplicate all files and folders to a target directory
 - Revise the extensions used to mark template files and folders
 - Complete the full implementation
 
 DONE:
 - Write a program to prompt the user for arguments
+- Write a program to walk the source repo and print out file/dir names
+- Modify the function to duplicate all files and folders to a target directory
--- go_imp/crest.go
+++ go_imp/crest.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -81,15 +83,72 @@ func buildProject(name string, frontend string, backend string, resources []stri
 	fmt.Printf("%v %v %v %v %v\n", name, frontend, backend, resources, components)
 }
 
+func walkFnWrapper(name string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+
+		// The directory where all the template files and folders are from
+		sourceBase := "/Users/developer/dev/code/projects/crest/go_imp/src"
+
+		cwd, getcwdError := os.Getwd()
+		if getcwdError != nil {
+			panic(getcwdError)
+		}
+
+		// The directory where all the template files and folders are going
+		targetBase := filepath.Join(cwd, name)
+
+		targetSuffix := strings.TrimPrefix(path, sourceBase)
+
+		// The path in the target directory for the new file or folder
+		newFilepath := filepath.Join(targetBase, targetSuffix)
+
+		if info.IsDir() {
+			fmt.Printf("mkdir %s\n", newFilepath)
+			mkdirErr := os.Mkdir(newFilepath, os.ModePerm)
+			if mkdirErr != nil {
+				panic(mkdirErr)
+			}
+		} else {
+			fmt.Printf("write file %s\n", newFilepath)
+			data, readFileErr := ioutil.ReadFile(path)
+			if readFileErr != nil {
+				panic(readFileErr)
+			}
+			writeFileErr := ioutil.WriteFile(newFilepath, data, os.ModePerm)
+			if writeFileErr != nil {
+				panic(writeFileErr)
+			}
+		}
+
+		return nil
+	}
+}
+
+func walkDir() {
+
+	// TODO: change this to be dynamic; build an executable and use os.executable's path
+	root := "/Users/developer/dev/code/projects/crest/go_imp/src"
+
+	walkErr := filepath.Walk(root, walkFnWrapper("myApp"))
+
+	if walkErr != nil {
+		panic(walkErr)
+	}
+
+}
+
 func main() {
 
-	fmt.Println("==== Crest - Create a RESTful project base ====")
-	name := promptSingle("What is your project name? ")
-	frontend := promptSingle("What is your project frontend? ", "react", "vue", "reframe", "angular")
-	backend := promptSingle("What is your project backend? ", "go", "flask", "node", "clojure")
-	resources := promptMulti("What are your project resources? ")
-	components := promptMulti("What are your project components? ")
+	// fmt.Println("==== Crest - Create a RESTful project base ====")
+	//// TODO: validate project name i.e. has legal characters
+	// name := promptSingle("What is your project name? ")
+	// frontend := promptSingle("What is your project frontend? ", "react", "vue", "reframe", "angular")
+	// backend := promptSingle("What is your project backend? ", "go", "flask", "node", "clojure")
+	// resources := promptMulti("What are your project resources? ")
+	// components := promptMulti("What are your project components? ")
 
-	buildProject(name, frontend, backend, resources, components)
+	// buildProject(name, frontend, backend, resources, components)
+
+	walkDir()
 
 }
