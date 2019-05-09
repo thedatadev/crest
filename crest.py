17b1f62b10284399d0ea2c2a29fc81a95d6981d1
thedatadev <thedatadev@gmail.com>
2019-05-09 10:10:57 +1000

    demo screencast

--- crest.py
+++ crest.py
@@ -3,7 +3,7 @@ import app_builder
 import sys
 import os
 
-FRONTEND_CHOICES = ['react', 'vue', 're-frame']
+FRONTEND_CHOICES = ['react', 'vue', 'reframe']
 BACKEND_CHOICES  = ['go', 'flask']
 
 frontend_prompt = f"Choose a frontend setup from: ({', '.join(FRONTEND_CHOICES)}): "
@@ -23,25 +23,25 @@ def prompt_for_multiple_choices(prompt_message):
     return input(prompt_message).split()
 
 if __name__ == "__main__":
-    
-    print("Crest - a full-stack RESTful project initialiser")
 
-    # app_name = input("App name: ")
+    print("Crest - a full-stack RESTful project builder")
 
-    # frontend_choice = prompt_for_single_choice(FRONTEND_CHOICES, frontend_prompt)
-    # backend_choice = prompt_for_single_choice(BACKEND_CHOICES, backend_prompt)
+    app_name = input("App name: ")
 
-    # resources = prompt_for_multiple_choices("List the REST resources for this project: ")
-    # components = prompt_for_multiple_choices("List the frontend components for this project: ")
+    frontend_choice = prompt_for_single_choice(FRONTEND_CHOICES, frontend_prompt)
+    backend_choice = prompt_for_single_choice(BACKEND_CHOICES, backend_prompt)
 
-    # items_to_interpolate = { 'resources': resources, 'components': components }
+    resources = prompt_for_multiple_choices("List the REST resources for this project: ")
+    components = prompt_for_multiple_choices("List the frontend components for this project: ")
 
-    app_name = "todomvc"
-    frontend_choice = "react"
-    backend_choice = "go"
-    resources = ["todo", "user"]
-    components = ["header", "form", "button"]
     items_to_interpolate = { 'resources': resources, 'components': components }
+
+    # app_name = "todomvc"
+    # frontend_choice = "react"
+    # backend_choice = "go"
+    # resources = ["todo", "user"]
+    # components = ["header", "form", "button"]
+    # items_to_interpolate = { 'resources': resources, 'components': components }
 
     app_builder.build('client', frontend_choice, items_to_interpolate)
     app_builder.build('server', backend_choice, items_to_interpolate)
